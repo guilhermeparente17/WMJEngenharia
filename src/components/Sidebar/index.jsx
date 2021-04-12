d895fcb01fe3ad54d4947c9c06a496557940bbbb
guilherme <guilherme.parente@outlook.com>
2021-04-12 18:26:05 -0300

    Adicionando o Sidebar

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { 
+    SidebarContainer, 
+    Icon, 
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu, 
+    SidebarLink, 
+    SideBtnWrap, 
+    SidebarRoute
+ } from './SidebarElements'
+
+const Sidebar = ({isOpen, toggle }) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about" onClick={toggle}>Sobre</SidebarLink>
+                    <SidebarLink to="discover" onClick={toggle}>O que é ?</SidebarLink>
+                    <SidebarLink to="services" onClick={toggle}>Obras</SidebarLink>
+                    <SidebarLink to="signup" onClick={toggle}>Inversores</SidebarLink>
+                    <SidebarLink to="signup" onClick={toggle}>Financiamentos</SidebarLink>
+                    <SidebarLink to="signup" onClick={toggle}>Contato</SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">Entre em Contato</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -11,6 +12,7 @@ const Home = () => {
     return (
         <div>
             <Navbar toggle={toggle} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
         </div>
     )
 }
